d72be86018aeb6043db26b2af3b9695d20135a01
weitao <51255903105@stu.ecnu.edu.cn>
2024-03-06 06:48:54 +0000

    init

--- python/dtas/__init__.py
+++ python/dtas/__init__.py
@@ -1,6 +1,8 @@
 from . import arch
 from .codegen.code_gen import CodeGenerator
 from .common.config import Config
+from . import schedule
+from . import policy
 # from .IRpass import *
 
 from .logging import get_log_level, set_log_level
